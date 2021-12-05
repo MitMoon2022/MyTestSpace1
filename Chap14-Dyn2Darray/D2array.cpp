16701c64c0d21e8f7fcee3c79b032c6bc4119eff
jeffiotan <jeffiotan@mit.com.sg>
2021-12-05 16:26:20 +0800

    ADD in comment for understanding

--- Chap14-Dyn2Darray/D2array.cpp
+++ Chap14-Dyn2Darray/D2array.cpp
@@ -4,14 +4,17 @@
 
 using namespace std;
 
-void fill(int **p, int rowSize, int columnSize);
+void fill(int **p, int rowSize, int columnSize);    //take note of the ptr2ptr
 void print(int **p, int rowSize, int columnSize);
 
 int main()
 {
-    int **board;        // Line 1
-    int rows;           // Line 2
-    int columns;        // Line 3
+    int **board;     //Data Type int, the variable(board) to be a pointer to pointer
+                    //board and *board are pointers.
+                    //board store the address of a ptr or array of ptr
+                    //*board store the address of an int memory space or an array of int values.
+    int rows;       // Line 2
+    int columns;    // Line 3
 
     cout << "Line 4: Enter the number of rows "
          << "and columns: ";                    // Line 4
@@ -19,21 +22,22 @@ int main()
     cout << endl;                               // Line 6
 
     // Create the rows of board
-    board = new int *[rows]; // Line 7
-    
+    board = new int *[rows];    //create  an array of pointers of type int.
+                                //assign the address of the array to board.
     // Create the columns of board
-    for (int row = 0; row < rows; row++)        // Line 8
-        board[row] = new int[columns];          // Line 9
+    for (int row = 0; row < rows; row++)  //For loop
+        board[row] = new int[columns];   //create the colums
     
     // Insert elements into board
-    fill(board, rows, columns);        // Line 10
-    cout << "Line 11: Board:" << endl; // Line 11
+    fill(board, rows, columns);        //ptr2ptr board is passed.
+    cout << "Ptr-2-Ptr: Board:" << endl; // Line 11
     
     // Output the elements of board
     print(board, rows, columns); // Line 12
     return 0;
 }
 
+//The function which fill in the matrix
 void fill(int **p, int rowSize, int columnSize)
 {
     for (int row = 0; row < rowSize; row++)
@@ -41,7 +45,7 @@ void fill(int **p, int rowSize, int columnSize)
         cout << "Enter " << columnSize << " number(s) for row "
              << "number " << row << ": ";
         for (int col = 0; col < columnSize; col++)
-            cin >> p[row][col];
+            cin >> p[row][col];     //able to use the [] array notation.
         cout << endl;
     }
 }
