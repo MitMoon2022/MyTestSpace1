a7ae3aee7df280facb14e643e25c3a5c7650eef5
jeffiotan <jeffiotan@mit.com.sg>
2022-05-30 11:26:46 +0800

    Add Console to find out the File.Copy method try and Catch exception.

---
+++ ConsoleFileCopy1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+
+
+namespace ConsoleFileCopy1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Start!");
+
+            // Creating two files
+            string sourceFile = @"file.txt";
+            string destinationFile = @"gfg.txt";
+            try
+            {
+                // Copying source file's contents to
+                // destination file
+                File.Copy(sourceFile, destinationFile, true);   //set overwrite to be true.
+            }
+            catch (IOException iox)
+            {
+                Console.WriteLine(iox.Message);
+            }
+            //---------------------------------------------
+            Console.WriteLine("Copying process has been done.");
+
+
+
+
+
+        }
+    }
+}
