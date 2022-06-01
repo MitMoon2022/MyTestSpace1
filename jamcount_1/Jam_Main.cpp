ac3c7acdd222fdce6890f061c6160b6c28069e0e
jeffiotan <jeffiotan@mit.com.sg>
2022-06-01 15:06:25 +0800

    Modify accordingly to the requirement. Read it up!

--- jamcount_1/Jam_Main.cpp
+++ jamcount_1/Jam_Main.cpp
@@ -20,7 +20,7 @@ struct Time {
   int secs;     //in my application case no need to use secs.
 };
 
-
+/**********************
 Time findTimeDifference(Time t1, Time t2){
     
     Time diff;
@@ -41,6 +41,82 @@ Time findTimeDifference(Time t1, Time t2){
      
     return diff;
 }
+****************/
+bool MonitorCheckTime(MonitorAlarm &RejTimeCheck, int iTimeLimit)
+{
+     CTime tLastOne = RejTimeCheck.stTimeRecord.back();     //*gets the timing of the Lastone.
+  //check all the time records
+  for(int i = 0; i < RejTimeCheck.iCurrCount; i++)
+ {
+      cout<<"print out iTime Hour of ["<<i<<"] = "<<RejTimeCheck.stTimeRecord[i].GetHour()<<endl;
+      cout<<"print out iTime Mins of ["<<i<<"] = "<<RejTimeCheck.stTimeRecord[i].GetMinute()<<endl;
+      cout<<"\n";
+      cout<<"print out iMiEXp Hour of["<<i<<"] ="<<RejTimeCheck.stMinExpire[i].GetHour()<<endl;
+      cout<<"print out iMiEXp Mins of["<<i<<"] ="<<RejTimeCheck.stMinExpire[i].GetMinute()<<endl;
+      cout<<"\n";
+      //---------------------------------------------------------------------------
+      
+     //Time Limit check
+    
+     CTimeSpan tspan = tLastOne - RejTimeCheck.stTimeRecord[i];             // Subtract 2 CTimes
+     long min = tspan.GetMinutes();
+
+     //Special Case:
+     if(i == RejTimeCheck.stTimeRecord.size()-1)
+     {
+         cout<<"Last count - Now the count i = "<<i<<endl;
+         cout<<"Clear all the vector records - StTimeRecord and stMinExpire"<<endl;
+         RejTimeCheck.stTimeRecord.clear();
+         RejTimeCheck.stMinExpire.clear();
+         break;
+     }
+
+       if(min >= iTimeLimit)
+        {
+         //clear the array Time elapsed is greater than the monitor Time.
+         cout<<"Time expired. min is more than the TimeLimit\n";
+         //RejTimeCheck.stTimeRecord.erase(RejTimeCheck.stTimeRecord.begin()+i);                      //v.erase(v.begin()+6);
+
+         //int sizeOfVect = m_Monitor3Dprobe.stTimeRecord.size();
+         //cout<<"Currently the size of the Vector is "<<sizeOfVect<<endl;
+         if (i >= RejTimeCheck.stTimeRecord.size()-1) //check that it reaches the last element then clear all.
+         {
+          //clear the all elements in vectors.
+          cout<<"Clear all the vector records - StTimeRecord and stMinExpire"<<endl;
+          RejTimeCheck.stTimeRecord.clear();
+          RejTimeCheck.stMinExpire.clear();
+         }
+         continue;                                       //return false;
+
+        }else  //lesser than the time limit
+           {
+                    //Both conditions Jam Count reached and Time limit are fulfilled.
+                    //output: AfxMessageBox("Jam Count and time limit reached!", MB_OK | MB_SYSTEMMODAL);
+                    //if (m_pDoc->GetUserLevel() > LOGIN_OPERATOR) to reset.
+
+                    cout<<"Print out - Jam Count and time limit hit! "<<endl;
+                    //vector<int>::iterator it = i;
+                   /* int j = i;
+                    while(j<RejTimeCheck.stTimeRecord.size()-1)
+                    {
+                        RejTimeCheck.stTimeRecord[j] = RejTimeCheck.stTimeRecord[j+1];
+                        RejTimeCheck.stMinExpire[j] = RejTimeCheck.stMinExpire[j+1];
+                        j++;
+                        cout<<"Transfer completed!\n";
+                    }*/
+                    //RejTimeCheck.stTimeRecord.pop_back();   //remove the TimeRecord last element.
+                    //RejTimeCheck.stMinExpire.pop_back();    //remove the MinExpire last element.
+                    //transfer the last element will become the first element.
+                    RejTimeCheck.stTimeRecord.erase(RejTimeCheck.stTimeRecord.begin(),RejTimeCheck.stTimeRecord.end()-1);
+                    RejTimeCheck.stMinExpire.erase(RejTimeCheck.stMinExpire.begin(), RejTimeCheck.stMinExpire.end()-1);
+                    cout<<"Now the size of RejTimeCheck is "<<RejTimeCheck.stTimeRecord.size()<<endl;
+                    return true;
+             }
+
+           
+ }//end_of_forLoop
+  return false;
+}//MonitorCheckTime() End 
 
 
 int main() {
@@ -48,7 +124,7 @@ int main() {
     const int m_iMaxCount = 5;
     //new user-defined parameters
    const int m_iUJamCount = 3;  //monitor the Consecutive MJ probe count limit it to 10 counts
-   const int m_iUJamTimeL = 2; //in mins limit
+   const int m_iUJamTimeL = 1; //in mins limit
    //------------------------------
    bool bEnd = false;
    bool m_bConsvFail;
@@ -65,7 +141,8 @@ int main() {
     cout<<"Start of Program"<<endl;
 
     m_bConsvFail = true;    //Consecutive fail is enabled
-    
+    bool bTriggerMonitor = false;
+
     while(!bEnd)
    {
     if(m_bConsvFail)
@@ -84,73 +161,23 @@ int main() {
             //int iMinutes = CurrentTime.GetMinute();
             //m_Monitor3Dprobe.stTimeRecord[ m_Monitor3Dprobe.iCurrCount-1] = CTime::GetCurrentTime(); //some time function to get the current time. 
             m_Monitor3Dprobe.stTimeRecord.push_back(CTime::GetCurrentTime());
+            m_Monitor3Dprobe.stMinExpire.push_back(m_Monitor3Dprobe.stTimeRecord.back() + CTimeSpan(0,0,m_iUJamTimeL,0));
+             
+            //m_Monitor3Dprobe.stTimeRecord[m_Monitor3Dprobe.iCurrCount]= CTime::GetCurrentTime();
 
             //m_Monitor3Dprobe.stMinExpire[ m_Monitor3Dprobe.iCurrCount-1]= m_Monitor3Dprobe.stTimeRecord[m_Monitor3Dprobe.iCurrCount-1] + CTimeSpan(0, 0, m_iUJamTimeL, 0);    // 1 hour later m_iUJamTimeL;
-            CTime TimeTemp = m_Monitor3Dprobe.stTimeRecord.back() + CTimeSpan(0, 0, m_iUJamTimeL, 0);
-            m_Monitor3Dprobe.stMinExpire.push_back(TimeTemp);
+            //CTime TimeTemp = m_Monitor3Dprobe.stTimeRecord[m_Monitor3Dprobe.iCurrCount] + CTimeSpan(0, 0, m_iUJamTimeL, 0);
+            //CTime TimeTemp = m_Monitor3Dprobe.stTimeRecord.back() + CTimeSpan(0, 0, m_iUJamTimeL, 0);
+            //m_Monitor3Dprobe.stMinExpire.push_back(TimeTemp);
+           
+            //m_Monitor3Dprobe.stMinExpire[m_Monitor3Dprobe.iCurrCount] = TimeTemp;
+            
         }
     //------------------------------------------------------------------------------------------------------------------------
         if( m_Monitor3Dprobe.iCurrCount == m_iUJamCount)    //Must inform user max is 10.
         {
-            //check all the time records
-            for(int i = 0; i < m_Monitor3Dprobe.iCurrCount; i++)
-            {
-                cout<<"print out iTime Hour of ["<<i<<"] = "<<m_Monitor3Dprobe.stTimeRecord[i].GetHour()<<endl;
-                cout<<"print out iTime Mins of ["<<i<<"] = "<<m_Monitor3Dprobe.stTimeRecord[i].GetMinute()<<endl;
-                cout<<"\n";
-                cout<<"print out iMiEXp Hour of["<<i<<"] ="<<m_Monitor3Dprobe.stMinExpire[i].GetHour()<<endl;
-                cout<<"print out iMiEXp Mins of["<<i<<"] ="<<m_Monitor3Dprobe.stMinExpire[i].GetMinute()<<endl;
-                cout<<"\n";
-            
-            //Time Limit check
-                //CTime tLastOne = m_Monitor3Dprobe.stTimeRecord[m_Monitor3Dprobe.iCurrCount-1];  //LastCount Timing which trigger 
-                CTime tLastOne = m_Monitor3Dprobe.stTimeRecord.back();
-                CTimeSpan tspan = tLastOne - m_Monitor3Dprobe.stTimeRecord[i];             // Subtract 2 CTimes
-                long min = tspan.GetMinutes();
-                cout<<"print out time minute interval is "<<min<<endl;
-            
-                if(min > m_iUJamTimeL)
-                {
-                    //clear the array Time elapsed is greater than the monitor Time.
-                    cout<<"Time expired\n";
-                    m_Monitor3Dprobe.stTimeRecord[i] = NULL;
+            bTriggerMonitor = MonitorCheckTime(m_Monitor3Dprobe, m_iUJamTimeL);
 
-                   //int sizeOfVect = m_Monitor3Dprobe.stTimeRecord.size();
-                   //cout<<"Currently the size of the Vector is "<<sizeOfVect<<endl;
-
-                    if (i >= m_Monitor3Dprobe.stTimeRecord.size())
-                    {
-                        //clear the all elements in vectors.
-                        m_Monitor3Dprobe.stTimeRecord.clear();
-                        m_Monitor3Dprobe.stMinExpire.clear();
-                    }
-
-                }else  //lesser than the time limit
-                {
-                    //Both conditions Jam Count reached and Time limit are fulfilled.
-                    //output: AfxMessageBox("Jam Count and time limit reached!", MB_OK | MB_SYSTEMMODAL);
-                    //if (m_pDoc->GetUserLevel() > LOGIN_OPERATOR) to reset.
-                    cout<<"Print out - Jam Count and time limit hit! "<<endl;
-
-                    //vector<int>::iterator it = i;
-                    int j = i;
-                    while(j<m_Monitor3Dprobe.stTimeRecord.size()-1)
-                    {
-                        m_Monitor3Dprobe.stTimeRecord[j] = m_Monitor3Dprobe.stTimeRecord[j+1];
-                        m_Monitor3Dprobe.stMinExpire[j] = m_Monitor3Dprobe.stMinExpire[j+1];
-                        j++;
-                        cout<<"Transfer completed!\n";
-                    }
-                     break;
-                }
-
-           
-            }//end_of_for
-
-
-
-            bEnd =true; //end the whileLoop
-            m_Monitor3Dprobe.iCurrCount =0;
         }//end_if_hit the jamcount
 
 
@@ -166,11 +193,38 @@ int main() {
 
            //}
 
-        
-        
-        
-        
-        
+        if(bTriggerMonitor)
+        {
+            cout<<"TriggerMonitor - True!\n";
+            bEnd =true; //end the whileLoop
+            m_Monitor3Dprobe.iCurrCount =0;     //reset count
+            break;
+        }else
+        {
+            cout<<"TiggerMonitor - False!\n";
+            if(m_Monitor3Dprobe.iCurrCount == m_iUJamCount)
+            {
+                 m_Monitor3Dprobe.iCurrCount =0;     //reset count
+                 /*vector <CTime>::iterator it1;
+                 it1 = m_Monitor3Dprobe.stTimeRecord.begin();
+
+                 for(int j = 0 ; j < m_Monitor3Dprobe.stTimeRecord.size(); j++)
+                 {
+                     if(CTime::GetCurrentTime() > m_Monitor3Dprobe.stMinExpire[j])
+                     {
+                         m_Monitor3Dprobe.stTimeRecord.erase(it1);
+                         m_Monitor3Dprobe.stMinExpire.erase(it1);
+                     }
+                     it1++;
+                 }*/
+
+                 m_Monitor3Dprobe.stTimeRecord.erase(m_Monitor3Dprobe.stTimeRecord.begin(),m_Monitor3Dprobe.stTimeRecord.end()-1);
+                 m_Monitor3Dprobe.stMinExpire.erase(m_Monitor3Dprobe.stMinExpire.begin(), m_Monitor3Dprobe.stMinExpire.end()-1);
+                 cout<<"Now the size of RejTimeCheck is "<<m_Monitor3Dprobe.stTimeRecord.size()<<endl;
+
+
+            }//end_if
+        }
 
 
     }//end_ifConsvFail
