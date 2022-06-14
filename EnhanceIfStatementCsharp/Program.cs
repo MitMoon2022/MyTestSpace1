fa17693824f994e5dabc9a5225e3f7b6f95e4c29
jeffiotan <jeffiotan@mit.com.sg>
2022-06-14 08:27:01 +0800

    EnhaceIf ? statement C#

---
+++ EnhanceIfStatementCsharp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace EnhanceIfStatementCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Program!");
+
+            int inputTemperature = 0;
+            string tempMess = string.Empty;
+            string inputValue = string.Empty;
+            //takes ip from console
+            Console.Write("Enter the current temperature : ");
+            inputValue = Console.ReadLine();
+
+            //Validate the ip.
+            bool validInteger = int.TryParse(inputValue, out inputTemperature);
+
+            // (condition) ? iftrue : iffalse;
+            if(validInteger)
+            {
+                tempMess = inputTemperature <= 15 ?
+                    // true
+                    "it is too cold here" :
+                    // false
+                    (inputTemperature >= 16 && inputTemperature <= 28) ?
+                    // true
+                    "it is cold here" :
+                    // false
+                    inputTemperature > 28 ? 
+                    // true
+                    "it is hot here" :
+                    // false
+                    "Empty";
+
+                Console.WriteLine(tempMess);
+            }
+            else
+            {
+
+                Console.WriteLine("Not a valid Temperature.");
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ XmlModelTest1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace XmlModelTest1
+{
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start of Program!");
+
+            XmlSerializer serializer = new XmlSerializer(typeof(TestModel));
+
+            //MemoryStream stream = new MemoryStream();
+            FileStream stream = File.OpenWrite("D:\\jftMit1\\test.xml");
+
+            serializer.Serialize(stream, new TestModel() { a1 = "test1", a2 = "test2", a3 = new List<string>() { "1", "2", "3" } });
+            stream.Dispose();
+
+            FileStream streamread = File.OpenRead("D:\\jftMit1\\test.xml");
+
+            var result = (TestModel)(serializer.Deserialize(streamread));
+
+            Console.Read();
+
+        }
+    }
+
+    public class TestModel
+    {
+        public string a1 { get; set; }
+        public string a2 { get; set; }
+        public List<string> a3 { get; set; }
+
+
+    }
+
+
+
+}
