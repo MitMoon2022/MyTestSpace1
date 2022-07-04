5e743d2132f479a0eb704482f24c6f7e8943d0d2
jeffiotan <jeffiotan@mit.com.sg>
2022-07-04 08:40:58 +0800

    Try out Basic/Singleton.cpp and Main.cpp.

--- Singleton/Basic/Singleton.cpp
+++ Singleton/Basic/Singleton.cpp
@@ -1,10 +1,15 @@
 #include "Singleton.h"
+#include <iostream>
+
+using namespace std;
 Singleton Singleton::m_Instance ;
 Singleton& Singleton::Instance() {
 	return m_Instance ;
 }
 
 void Singleton::MethodA() {
+
+	cout << "MethodA" << endl;
 }
 
 void Singleton::MethodB() {
--- Singleton/Basic/main.cpp
+++ Singleton/Basic/main.cpp
@@ -1,7 +1,11 @@
 #include "Singleton.h"
+#include <iostream>
 #include <corecrt_math.h>
 
+using namespace std;
+
 int main() {
+	cout << "Start Program.\n";
 	Singleton &s = Singleton::Instance() ;
 	s.MethodA() ;
 
