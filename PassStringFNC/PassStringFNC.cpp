350902f4083dc2cd352272f57558b89d59864bfd
jeffiotan <jeffiotan@mit.com.sg>
2022-07-28 12:10:50 +0800

    Testing the ARC KeyValue string function in PassStringFNC.cpp Str1 =  Str2 is possible.

--- PassStringFNC/PassStringFNC.cpp
+++ PassStringFNC/PassStringFNC.cpp
@@ -11,37 +11,101 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+#include <vector>
+#include "KeyValue.h"
 
 //using namespace std::basic_istream;
 
 using namespace std;
+
+enum ARC_STATUS_SVID
+{
+    SVID_SYSTEMREADY,
+    SVID_ALARMSTATE,
+    SVID_REELBARCODE,
+    SVID_GOODREELQTY,
+    SVID_NGREELQTY,
+    SVID_UNITREELQTY,
+    MAX_STATUS_SVID
+};
+
+const string m_ArrStatusSVID[MAX_STATUS_SVID] =
+{
+    "SYSTEMREADY",
+    "ALARMSTATE",
+    "REELBARCODE",
+    "GOODREELQTY",
+    "NGREELQTY",
+    "UNITREELQTY",
+};
+
+
 
 void display(char *);
 void display(string);
 
 int main()
 {
-    cout << "Hello World!\n";
-    string str1;
-    char str[100];
+    //cout << "Hello World!\n";
+    //string str1;
+    //char str[100];
 
-    cout << "Enter a string: ";
-    getline(cin, str1);
+    //cout << "Enter a string: ";
+    //getline(cin, str1);
 
-    cout << "Enter another string: ";
-    cin.get(str, 100, '\n');
+    //cout << "Enter another string: ";
+    //cin.get(str, 100, '\n');
 
-    display(str1);
-    display(str);
-    //-----------------------------------------
-    // The string member function c_str() will return the string in the form of a char* (with a null-terminator).
-    cout << "test of c_str()\n";
-    str1.c_str();   //Convert std::string to const char*
-    cout << str1;
-    cout << "<-Test1 where it is joined\n";
+    //display(str1);
+    //display(str);
+    ////-----------------------------------------
+    //// The string member function c_str() will return the string in the form of a char* (with a null-terminator).
+    //cout << "test of c_str()\n";
+    //str1.c_str();   //Convert std::string to const char*
+    //cout << str1;
+    //cout << "<-Test1 where it is joined\n";
 
-    cout << str1.size() << endl;
+    //cout << str1.size() << endl;
 
+    //==============================ARC SendCMD============================================
+    
+    int nStatus = 0;
+    string strRet = "GOODREELQTY;555";
+
+    CKeyValue keyValue;
+    std::vector<std::string> vecToken;
+    vecToken = keyValue.split(strRet, ';');
+
+    //using strcmp function which compare two strings, return:
+    //0	 if strings are equal
+    //>0 if the first non-matching character in str1 is greater (in ASCII) than that of str2.
+    //<0 if the first non - matching character in str1 is lower(in ASCII) than that of str2.
+    if (vecToken.size() >= 2 && strcmp(vecToken[0].c_str(), m_ArrStatusSVID[SVID_SYSTEMREADY].c_str()) == 0)
+    {
+        nStatus = stoi(vecToken[1]);
+    }
+    if (vecToken.size() >= 2 && strcmp(vecToken[0].c_str(), m_ArrStatusSVID[SVID_GOODREELQTY].c_str()) == 0)
+    {
+        nStatus = stoi(vecToken[1]);
+    }
+    if (vecToken.size() >= 2 && strcmp(vecToken[0].c_str(), m_ArrStatusSVID[SVID_NGREELQTY].c_str()) == 0)
+    {
+        nStatus = stoi(vecToken[1]);
+    }
+    if (vecToken.size() >= 2 && strcmp(vecToken[0].c_str(), m_ArrStatusSVID[SVID_UNITREELQTY].c_str()) == 0)
+    {
+        nStatus = stoi(vecToken[1]);
+    }
+
+
+
+    cout << "nStatus is " << nStatus << endl;
+
+    string st1 = "Happy";
+
+    string st2 = st1;
+
+    cout << "st2 is " << st2 << endl;
 
 
     return 0;
