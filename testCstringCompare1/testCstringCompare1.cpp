73d6647d04822fbba988e26d05e5fd3121ee5c7b
jeffiotan <jeffiotan@mit.com.sg>
2022-08-15 17:46:34 +0800

    Assert about on CString CompareNoCase.

--- testCstringCompare1/testCstringCompare1.cpp
+++ testCstringCompare1/testCstringCompare1.cpp
@@ -5,6 +5,12 @@
 #include <CString>
 
 #include "stdafx.h"
+//#include "afxstr.h"
+#include "atlstr.h"
+#include <cassert>
+#include <assert.h>
+
+
 
 
 using namespace std;
@@ -16,8 +22,13 @@ int _tmain(int argc, _TCHAR* argv[])
    // example for CString::CompareNoCase
     CString s1( "abc" );
     CString s2( "ABD" );
-    ASSERT( s1.CompareNoCase( s2 ) == -1 ); // Compare with a CString.
-    ASSERT( s1.Compare( "ABE" ) == -1 ); // Compare with LPTSTR string.
+    //CompareNoCase - return 0 when it is true!
+    assert( s1.CompareNoCase( s2 ) == -1 ); // Compare with a CString.
+    //assert(Expression)
+    //Expression to be evaluated. If this expression evaluates to 0 (True), this causes an assertion failure that terminates the program.
+    assert( s1.Compare( "ABE" ) < 0 ); // Compare with LPTSTR string.
+    int error = s1.Compare( "ABE" );    //return 1
+    cout<<"error is "<<error<<endl;
 
     system("pause");
 	return 0;
