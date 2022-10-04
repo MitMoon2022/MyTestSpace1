981c7c5714180655beb757c9d18b4dd82322b752
jeffiotan <jeffiotan@mit.com.sg>
2022-10-04 14:23:54 +0800

    AddOn: Learn about vector<int *> or vector<class *> and how to access its element or address of vector's elements or address of locations of vector

--- VectorEraseFrontBack/VectorEraseFront.cpp
+++ VectorEraseFrontBack/VectorEraseFront.cpp
@@ -54,12 +54,61 @@ int main()
         cout<<vtr.back()<<endl; //a dedicated back() which return the element.
         vtr.pop_back();  //remove the last element.
 
-
         //-------------------------------------------------------------
         for(vector<char>::iterator it = vtr.begin(); it != vtr.end(); it++)
             cout << *it << ' ';
         cout << endl;
         //-------------------------------------------------------------
+        //Date: 04/10/2022
+        //https://www.delftstack.com/howto/cpp/cpp-vector-of-pointers/
+        //
+  /*    std::vector offers rich functionality to allocate a vector of pointers and 
+        manipulate the vector with multiple built-in functions. 
+        This method provides a more flexible interface for new element creation during
+        the run-time. Notice that we initialized the vector elements with the nullptr value
+        as shown:
+  */
+        //Start
+        cout<<"==========================================================================="<<endl;
+        vector<int*> vector_p;
+
+        vector_p.push_back(new int(1)); //new operator returns a pointer to the heap.
+        vector_p.push_back(new int(2));
+        vector_p.push_back(new int(3));
+        vector_p.push_back(new int(4));
+        vector_p.push_back(new int(5));
+
+        //auto count = vector_p.size();
+
+        cout << "vector_p addresses:" << endl;
+        for(int i = 0; i < vector_p.size(); ++i) {
+            cout << &vector_p[i] << endl;
+        }
+        cout<<endl;
+
+        //How to access the content of the vector's element of pointers *variable.
+         cout << "vector_p content:" << endl;
+        for(int i = 0; i < vector_p.size(); ++i) {
+            cout << *vector_p[i] << endl;
+        }
+        cout<<endl;
+
+         cout << "vector_p XXXX:" << endl;
+        for(int i = 0; i < vector_p.size(); ++i) {
+            cout << vector_p[i] << endl;
+        }
+        cout<<endl;
+
+
+
+
+        /*for(int*& s : vector_p)
+            cout<< s <<endl
+
+        cout<<endl;*/
+
+
+
 
     system("pause");
 	return 0;
