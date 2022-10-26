3d669176cc4090b6364b9d8421e42ab174f9b5e0
jeffiotan <jeffiotan@mit.com.sg>
2022-10-26 14:49:43 +0800

    Design Pattern Builder from Dmitri Nesteruk. Downloaded.

---
+++ DP_Facet/DP_Facet.cpp
@@ -0,0 +1,14 @@
+// DP_Facet.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main01()    //Not running main if you append 00 or underscore__
+{
+    std::cout << "Hello World!\n";
+
+    std::cout<<"It is running \"Facets.cpp.\""<<std::endl;
+
+    return 0;
+}
+
